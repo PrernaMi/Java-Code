89a8be6aab48fa90fb2de6f5d43d1ab3cb894829
Prerna <125128710+PrernaMi@users.noreply.github.com>
2023-10-18 16:58:19 +0530

    Add files via upload

---
+++ Trees/Node_Height.java
@@ -0,0 +1,37 @@
+package Trees;
+
+public class Node_Height {
+	
+	public static class Node{
+		int data;
+		Node left;
+		Node right;
+		Node(int data){
+			this.data = data;
+		}
+	}
+	
+	public static int height(Node root) {
+		if(root == null)return 0;
+		return 1+Math.max(height(root.left), height(root.right));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node root = new Node(1);
+		Node a = new Node(2);
+		Node b = new Node(3);
+		root.left = a;
+		root.right = b;
+		Node c = new Node(35);
+		Node d = new Node(5);
+		a.left = c;
+		a.right = d;
+		Node e = new Node(6);
+		b.right = e;
+		int result = height(root);
+		System.out.println(result);
+
+	}
+
+}
