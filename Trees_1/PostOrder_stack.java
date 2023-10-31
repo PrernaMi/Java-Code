99b6733c7b947a41c1705555202614e276be39a3
Prerna <125128710+PrernaMi@users.noreply.github.com>
2023-10-31 16:56:39 +0530

    Add files via upload

---
+++ Trees_1/PostOrder_stack.java
@@ -0,0 +1,58 @@
+package Trees_1;
+
+import java.util.Stack;
+
+import Trees_1.InOrder_Stack.Node;
+
+public class PostOrder_stack {
+	
+	public static class Node{
+		int data;
+		Node left;
+		Node right;
+		Node(int data){
+			this.data = data;
+		}
+	}
+	
+	public static void bfs(Node root) {
+		Stack<Node> st = new Stack<Node>(); 
+		if(root != null)st.push(root);
+		while(st.size() > 0) {
+			Node temp = st.peek();
+			if(temp.left != null) {
+				st.push(temp.left);
+				temp.left = null;
+			}
+			else if(temp.right != null) {
+				st.push(temp.right);
+				temp.right = null;
+			}else {
+				st.pop();
+				System.out.print(temp.data+" ");
+				
+			}
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node root = new Node(1);
+		Node a = new Node(2);
+		Node b = new Node(3);
+		root.left = a;
+		root.right = b;
+		Node c = new Node(4);
+		Node d = new Node(5);
+		a.left = c;
+		a.right = d;
+		Node e = new Node(6);
+		Node f = new Node(7);
+		b.left = e;
+		b.right = f;
+		bfs(root);
+
+	}
+
+}
